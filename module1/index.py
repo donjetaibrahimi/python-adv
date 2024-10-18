9053e393862f47c149452d362a3c3d77d91269de
Donjeta <idonjeta8@gmail.com>
2024-10-18 17:00:53 +0200

    added first file

---
+++ module1/index.py
@@ -0,0 +1,26 @@
+emri = "donjeta"
+mosha =17
+datelindja = 27/10
+
+
+print(emri)
+print(type(emri))
+print(type(mosha))
+print(type(datelindja))
+
+mosha=mosha+1
+print(mosha)
+
+emri_mbiemri = emri + "ibrahimi"
+print(emri_mbiemri)
+
+my_data=["donjeta", "ibrahimi", 18, "ma e mira"]
+print(my_data[2])
+my_data.append("prishtina")
+print(my_data)
+my_data.insert(4,"fakulltet")
+print(my_data)
+my_data.remove(18)
+print(my_data)
+del my_data[2]
+print(my_data)
